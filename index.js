a9edcba8f1a789b47f52fe7f779a4b6cfa9e8c38
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-12-20 16:23:32 -0300

    generar punto de entrada principal y conexion con base de datos

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import selectionRouter from "./app/router/selectionRouter.js"
+import { dbConn } from "./app/config/db.js";
+
+configDotenv();
+const {
+    PORT = 3000,
+    URI = 'mongodb://localhost:27017/test'
+} = process.env;
+const app = express();
+dbConn(URI)
+
+app.use("/selection", selectionRouter);
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(`service running on port ${PORT}`))
